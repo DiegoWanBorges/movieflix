9f9c42bf0f1c6ca5b8d31a03d53ad19825a7be19
diegowanborges <diegowanborges@gmail.com>
2021-03-22 11:04:37 -0300

    Entitie Movie

--- backend/src/main/java/com/devsuperior/movieflix/entities/Genre.java
+++ backend/src/main/java/com/devsuperior/movieflix/entities/Genre.java
@@ -1,12 +1,17 @@
 package com.devsuperior.movieflix.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+
+
 
 @Entity
 @Table(name="tb_genre")
@@ -17,6 +22,8 @@ public class Genre implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
+	@OneToMany(mappedBy = "gender")
+	private List<Movie> movies = new ArrayList<>();
 	
 	public Genre(){
 		
@@ -41,6 +48,10 @@ public class Genre implements Serializable {
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	public List<Movie> getMovies() {
+		return movies;
 	}
 
 	@Override
