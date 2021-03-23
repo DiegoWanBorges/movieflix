fcd21348ef2bb828cc77bafd5c54db15e356f188
diegowanborges <diegowanborges@gmail.com>
2021-03-23 18:37:11 -0300

    Added 'Eyes open and closed' in input logon

--- front-web/src/pages/Login/Components/LoginCard/index.tsx
+++ front-web/src/pages/Login/Components/LoginCard/index.tsx
@@ -1,9 +1,13 @@
 import './styles.scss'
 import { ReactComponent as Arrow } from '../../../../core/assets/images/arrow.svg'
+import EyesOpened  from '../../../../core/assets/images/eyesOpened.png'
+import EyesClosed  from '../../../../core/assets/images/eyesClosed.png'
+import { useState } from 'react'
 
 const LoginCard = () => {
+    const [hidePassword, setHidePassword]=useState(false);
     return (
-        <form className="card-login-main">
+        <div className="card-login-main">
             <h1 className="card-login-title">
                 LOGIN
            </h1>
@@ -14,19 +18,27 @@ const LoginCard = () => {
             />
             <input
                 className="card-login-input card-login-password"
-                type="password"
+                type={hidePassword ? "text" : "password"}
                 placeholder="Senha"
             />
+            <button 
+                className="card-login-hide-password" 
+                onClick={() => setHidePassword(!hidePassword)}
+            >
+                <img src={hidePassword ?  EyesClosed : EyesOpened} alt="" />
+            </button>
 
             <div className="card-login-button">
                 <button className="card-login-btn-icon">
                     LOGAR
                 </button>
+                
+                
                 <div className="card-login-btn-incon-content">
                     <Arrow />
                 </div>
             </div>
-        </form>
+        </div>
     )
 }
 export default LoginCard;
