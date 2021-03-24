69551c99eb05fca8e4f13e0deb9d8b09455a5b42
diegowanborges <diegowanborges@gmail.com>
2021-03-24 15:32:47 -0300

    Added movie card

--- front-web/src/pages/Movies/index.tsx
+++ front-web/src/pages/Movies/index.tsx
@@ -1,9 +1,54 @@
+import { useEffect, useState } from 'react';
+import { MoviesResponse } from '../../core/types/Movie';
+import { makePrivateRequest } from '../../core/utils/request';
+import MovieCard from './Components/MovieCard'
+
 import './styles.scss'
 
 const Movies = () =>{
+    const [moviesResponse, setMoviesResponse] = useState<MoviesResponse>();
+
+    
+
+    useEffect(() =>{
+        const params = {
+            linesPerPage: 12,
+        }
+        makePrivateRequest({ url: '/movies', params })
+            .then(response => setMoviesResponse(response.data))
+            .finally(() => {
+                
+            })
+    },[])
+
     return (
-        <div >
-            <h1>Movies</h1>
+        <div className="movie-main">
+            
+            <div className="movie-filter">
+                
+            </div>
+
+            <div className="movie-list">
+                {
+                    moviesResponse?.content.map(movie =>(
+                        <MovieCard movie={movie} />
+                    ))
+                }
+
+              
+                
+            </div>
+            
+            <div className="movie-list">
+
+            </div>
+            
+
+
+
+
+
+
         </div>
     )
 }
