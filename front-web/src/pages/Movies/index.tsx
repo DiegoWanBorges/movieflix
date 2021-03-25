f685bdf61425d8925dc6ec3f1df35f02151c8c8b
diegowanborges <diegowanborges@gmail.com>
2021-03-25 11:13:13 -0300

    Added movieFilter

--- front-web/src/pages/Movies/index.tsx
+++ front-web/src/pages/Movies/index.tsx
@@ -1,54 +1,48 @@
 import { useEffect, useState } from 'react';
-import { MoviesResponse } from '../../core/types/Movie';
+import { Genre, MoviesResponse } from '../../core/types/Movie';
 import { makePrivateRequest } from '../../core/utils/request';
 import MovieCard from './Components/MovieCard'
+import MovieFilter from './Components/MovieFilter';
 
 import './styles.scss'
 
-const Movies = () =>{
+const Movies = () => {
     const [moviesResponse, setMoviesResponse] = useState<MoviesResponse>();
-
-    
-
-    useEffect(() =>{
+    const [genre, setGenre] = useState<Genre>();
+    const [activePage, setActivePage] = useState(0);
+    useEffect(() => {
         const params = {
             linesPerPage: 12,
+            genreId: genre?.id
         }
         makePrivateRequest({ url: '/movies', params })
             .then(response => setMoviesResponse(response.data))
             .finally(() => {
-                
             })
-    },[])
+    }, [genre])
+
+    const handleChangeGenre = (genre: Genre) => {
+        console.log(genre)
+        setActivePage(0);
+        setGenre(genre);
+
+    }
 
     return (
         <div className="movie-main">
             
-            <div className="movie-filter">
-                
-            </div>
+            <MovieFilter
+                genre={genre}
+                handleChangeGenre={handleChangeGenre}
+            />
 
             <div className="movie-list">
                 {
-                    moviesResponse?.content.map(movie =>(
+                    moviesResponse?.content.map(movie => (
                         <MovieCard movie={movie} />
                     ))
                 }
-
-              
-                
             </div>
-            
-            <div className="movie-list">
-
-            </div>
-            
-
-
-
-
-
-
         </div>
     )
 }
